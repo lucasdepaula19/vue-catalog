2016f5d98c9c410fc08e08a0933b0f21cf172b93
lucasdepaula19 <lucasdepaula2012@gmail.com>
2020-07-11 00:14:15 -0300

    primeiros passos

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <!-- <h1>{{ msg }}</h1> -->
+    <b-form class="busca" method="post" action="/busca">
+      <b-input-group>
+        <b-input
+          class="input-buscar"
+          type="text"
+          placeholder="Para onde você quer ir?"
+          name="termo"
+          id="texto-buscar-home"
+          autocomplete="off"
+        />
+        <b-button variant="info" type="submit" class="btn-buscar">Buscar</b-button>
+      </b-input-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchComponent",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* Usar sempre CSS 'flex' */
+.busca {
+  width: 35rem;
+  margin: 0;
+}
+.container {
+  height: 7em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.btn-buscar {
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  width: 6em;
+}
+.input-buscar {
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    
+    <!-- <img alt="Vue logo" src="../../image/logo.png"> -->
+    <!-- <SearchComponent msg="Welcome to Your Vue.js App"/> -->
+    <h1>Para onde irei?</h1>
+    <SearchComponent msg=""/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SearchComponent from '@/components/SearchComponent.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SearchComponent
   }
 }
 </script>
+<style scoped>
+.home{
+
+  background-image: url("../assets/scotland-540119_1920.jpg");
+  height: 100%;
+  background-size: cover;
+}
+</style>
