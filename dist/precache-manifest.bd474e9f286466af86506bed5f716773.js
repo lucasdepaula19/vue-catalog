3e43079ca84c7c579e8322d5c884e260812b48df
lucasdepaula19 <lucasdepaula2012@gmail.com>
2020-07-12 18:35:58 -0300

    ajustes heroku

---
+++ dist/precache-manifest.bd474e9f286466af86506bed5f716773.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a4b317f22eac736b5c",
+    "url": "/css/app.08ae1947.css"
+  },
+  {
+    "revision": "65a3f30f57fa8f1b773a",
+    "url": "/css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "0364a1246593ab249e2c3356d4dbe0fb",
+    "url": "/img/scotland-540119_1920.0364a124.jpg"
+  },
+  {
+    "revision": "0364a1246593ab249e2c3356d4dbe0fb",
+    "url": "/img/scotland-540119_1920.jpg"
+  },
+  {
+    "revision": "9ef62cbd5fddb933c80ff90cee7e05a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86a4b317f22eac736b5c",
+    "url": "/js/app.122ec57d.js"
+  },
+  {
+    "revision": "65a3f30f57fa8f1b773a",
+    "url": "/js/chunk-vendors.7d3765c3.js"
+  },
+  {
+    "revision": "2182a8fe348178f1df2d99d292c01285",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
